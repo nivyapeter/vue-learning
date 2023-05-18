7757570934b570c45f35aab1c2417c97447d15e3
nivya <nivya.peter@vonnue.com>
2023-05-18 15:40:34 +0530

    feat: conditional rendering

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,30 +1,15 @@
 <template>
 <div>
-<div>{{greet}}{{name}}</div>
-<div v-text="channel"></div>
-<div v-html='channel'></div>
-<h2 v-bind:id='headingId'>heading</h2>
-<button :disabled='isDisable'>Bind</button>
-<h2 class='underline'>underline text</h2>
-<h2 :class='status'>status</h2>
-<h2 :class="isPromoted && 'promoted'">Promoted</h2>
-<h2 :class="isSoldOut ? 'sold-out' : 'new'">Solout?: movie</h2>
-<h2 :class="isSoldOut ? ['new','promoted'] : ['sold-out']">Newly promoted movie</h2>
+<h2 v-if="num === 0">The number is zero</h2>
+<h2 v-else-if="num<0">The number is negative</h2>
+<h2 v-else-if="num>0">The number is positive</h2>
+<h2 v-else>Not a number</h2>
 
-<h2 :class="[isPromoted && 'promoted',isSoldOut ? 'sold-out' : 'new']">Array conditional</h2>
+<div v-if='display'>
+  <h2>haiiiii</h2>
+</div>
 
-<h2 :class="{
-  promoted: isPromoted,
-  new: !isSoldOut,
-  'sold-out':isSoldOut
-  }">object conditional</h2>
-
-<h2 :style="{color:highLightColor,fontSize:headerSize + 'px'}">Inline style</h2>
-
-<h2 :style='headerStyleObject'>style object</h2>
-
-<div :style="[baseStyleObject,successStyleObject]">Success style</div>
-
+<h2 v-show='showElement'>using v-show property</h2>
 
 </div>
 </template>
@@ -34,34 +19,12 @@ export default {
   name: 'App',
   data() {
     return {
-      greet: 'hello',
-      name:'Nivya',
-      channel:'<b>code evolution</b>',
-      headingId: 'heading',
-      isDiasbled: false,
-      status: 'danger',
-      isPromoted: false,
-      isSoldOut: true,
-      highLightColor: 'orange',
-      headerSize:50,
-      headerStyleObject: {
-        color: 'red',
-        padding:'20px',
-        fontSize:'60px'
-      },
-      baseStyleObject: {
-        fontSize:'50px',
-        padding:'10px'
-      },
-      successStyleObject: {
-        color:'green',
-        backgroundColor: 'lightgreen',
-        border: '1px solid green',
-        padding:'20px'
-      }
-
+      num: '',
+      display:true,
+      showElement: true
     }
   }
+
 }
 </script>
 
