f5698d26cec9835f01ef3694e5fdf23b843eadbf
nivya <nivya.peter@vonnue.com>
2023-05-22 14:57:33 +0530

    feat: computed setter in vue

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -5,6 +5,9 @@
   <h2 v-pre>{{name}}</h2>
   <h2>Full name: {{firstName}}{{lastName}}</h2>
   <h2>Computed fullName: {{fullName}}</h2>
+
+  <button @click="changeFullName">Change fullName</button>
+
   <!-- <h2>Total = {{items.reduce((total,current) => (total = total + current.price),0)}}</h2> -->
   <button @click="items.push({id:4,title:'watch',price:150})">Add item</button>
   <h2>Computed Total = {{total}}</h2>
@@ -50,12 +53,23 @@ export default {
   getTotal() {
     console.log('getTotal method');
       return this.items.reduce((total,current) => (total + current.price),0)
-    }
+    },
+  changeFullName() {
+   this.fullName = 'anju thomas'
+  }
   },
   computed:{
-    fullName() {
+    fullName: {
+      get() {
       return `${this.firstName} ${this.lastName} `   
+      },
+      set(value) {
+        const names = value.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[1]
+      }
     },
+
     total() {
       console.log('computed total');
       return this.items.reduce((total,current) => (total + current.price),0)
