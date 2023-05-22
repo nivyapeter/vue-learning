c4f256a2febe59abc1899e0726a79ec69ffeae95
nivya <nivya.peter@vonnue.com>
2023-05-22 12:47:26 +0530

    feat: computed properties in vue

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -3,6 +3,11 @@
   <h2 v-once>{{name}}</h2>
   <button @click="name='batsman'">Change name</button>
   <h2 v-pre>{{name}}</h2>
+  <h2>Full name: {{firstName}}{{lastName}}</h2>
+  <h2>Computed fullName: {{fullName}}</h2>
+  <!-- <h2>Total = {{items.reduce((total,current) => (total = total + current.price),0)}}</h2> -->
+  <button @click="items.push({id:4,title:'watch',price:150})">Add item</button>
+  <h2>Computed Total = {{total}}</h2>
 </div>
 </template>
 
@@ -11,7 +16,37 @@ export default {
   name: 'App',
   data() {
     return {
-      name: 'viswas'
+      name: 'viswas',
+      firstName: 'nivya',
+      lastName:'peter',
+      items:[
+        {
+          id: 1,
+          title:'tv',
+          price: 100,
+        },
+        {
+          id: 1,
+          title:'phone',
+          price: 200,
+        },
+        {
+          id: 1,
+          title:'laptop',
+          price: 300,
+        }
+      ]
+    }
+  },
+  methods: {
+ 
+  },
+  computed:{
+    fullName() {
+      return `${this.firstName} ${this.lastName} `   
+    },
+    total() {
+      return this.items.reduce((total,current) => (total + current.price),0)
     }
   }
   
