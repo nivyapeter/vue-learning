2d96ef3231a15bcc237e9f02bf83c9801e07c425
nivya <nivya.peter@vonnue.com>
2023-05-22 16:22:59 +0530

    feat: basic component structure in vue

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,53 +1,13 @@
 <template>
-  <div>
-    <h2>volume tracker (0-20)</h2>
-    <h3>Current volume - {{ volume }}</h3>
-    <div>
-      <button @click="volume += 2">Increase</button>
-      <button @click="volume -= 2">Decrease</button>
-    </div>
-    <input type="text" v-model="movie" />
-    <input type="text" v-model="movieInfo.title" />
-    <input type="text" v-model="movieInfo.actor" />
-  </div>
+  <GreetSection />
 </template>
 
 <script>
+import GreetSection from "./components/GreetSection.vue";
 export default {
   name: "App",
-  data() {
-    return {
-      volume: 0,
-      movie: "batsman",
-      movieInfo: {
-        title: "",
-        actor: "",
-      },
-    };
-  },
-  methods: {},
-  computed: {},
-  watch: {
-    volume(newValue) {
-      if (newValue === 16) {
-        alert("Volume is high");
-      }
-    },
-
-    movie: {
-      handler(newValue) {
-        console.log(`Calling API WITH MOVIE NAME = ${newValue}`);
-      },
-      immadiate: true,
-    },
-    movieInfo: {
-      handler(newValue) {
-        console.log(
-          `Calling API WITH MOVIE NAME = ${newValue.title} ${newValue.actor}`
-        );
-      },
-      deep: true,
-    },
+  components: {
+    GreetSection,
   },
 };
 </script>
