c446c17025907808109bb5ecb10301e6641b59d7
nivya <nivya.peter@vonnue.com>
2023-05-23 14:44:00 +0530

    feat: validating emitted events

--- hello-world/src/components/PopupSection.vue
+++ hello-world/src/components/PopupSection.vue
@@ -1,14 +1,28 @@
 <template>
   <div>
     <h2>This is a Popup</h2>
-    <button @click="$emit('close', 'vswas')">Close popup</button>
+    <input type="text" v-model="name" />
+    <button @click="$emit('close', name)">Close popup</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "PopupSection",
-  emits: ["close"],
+  emits: {
+    close: (name) => {
+      if (!name) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+  },
+  data() {
+    return {
+      name: "",
+    };
+  },
 };
 </script>
 
