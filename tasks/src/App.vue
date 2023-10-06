9b525bfcf6a42951de7ef92e39764fd0d3f09d07
nivya <nivya.peter@vonnue.com>
2023-10-06 17:43:11 +0530

    feat: background scroll effect

--- tasks/src/App.vue
+++ tasks/src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <div>
-    <filter-list />
+    <!-- <filter-list />
     <ArrayNumber />
-    <FindIndex />
+    <FindIndex /> -->
+    <BackgroundScrollEffect />
   </div>
 </template>
 
 <script>
-import ArrayNumber from './components/ArrayNumber.vue';
-import FilterList from './components/FilterList.vue';
+// import ArrayNumber from './components/ArrayNumber.vue';
+// import FilterList from './components/FilterList.vue';
+import BackgroundScrollEffect from './components/BackgroundScrollEffect.vue';
 
 export default {
   name: 'App',
   components: {
-    ArrayNumber,
-    FilterList
+    // ArrayNumber,
+    // FilterList
+    BackgroundScrollEffect
 }
 }
 </script>
@@ -26,6 +29,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 </style>
