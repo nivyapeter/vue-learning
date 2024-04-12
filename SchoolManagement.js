f3b8ea2f95227111122f7b91b6648df3843939f0
nivya <nivya.peter@vonnue.com>
2024-04-12 13:07:29 +0530

    feat: add array methods related to school management

---
+++ SchoolManagement.js
@@ -0,0 +1,67 @@
+let classObj = {
+    "name": "class A",
+    "teacherName": "Mary",
+    "students": [
+        {
+            "name": "Ravi",
+            "id": "101",
+            "marks": [
+                { "subject": "English", "mark": 25 },
+                { "subject": "Maths", "mark": 48 },
+                { "subject": "Physics", "mark": 40 },
+                { "subject": "Chemistry", "mark": 30 },
+                { "subject": "Computer", "mark": 20 }
+            ]
+        },
+        {
+            "name": "Aju",
+            "id": "102",
+            "marks": [
+                { "subject": "English", "mark": 35 },
+                { "subject": "Maths", "mark": 38 },
+                { "subject": "Physics", "mark": 33 },
+                { "subject": "Chemistry", "mark": 34 },
+                { "subject": "Computer", "mark": 30 }
+            ]
+        },
+        {
+            "name": "Mini SS",
+            "id": "103",
+            "marks": [
+                { "subject": "English", "mark": 12 },
+                { "subject": "Maths", "mark": 49 },
+                { "subject": "Physics", "mark": 18 },
+                { "subject": "Chemistry", "mark": 30 },
+                { "subject": "Computer", "mark": 40 }
+            ]
+        },
+        {
+            "name": "Binu",
+            "id": "104",
+            "marks": [
+                { "subject": "English", "mark": 49 },
+                { "subject": "Maths", "mark": 49 },
+                { "subject": "Physics", "mark": 47 },
+                { "subject": "Chemistry", "mark": 46 },
+                { "subject": "Computer", "mark": 50 }
+            ]
+        }
+    ]
+}
+
+const namesOfAllTheStudentsInTHeClass = () => {
+    return classObj.students.map((student) => student.name).toString()
+}
+
+const idsOfAllTheStudentsInTHeClass = () => {
+    return classObj.students.map((student) => student.id).toString()
+}
+
+const subjectsOfASpecificStudent = (name) =>  classObj.students.filter((student) => student)
+
+
+console.log(classObj.name);
+console.log(classObj.teacherName);
+console.log(namesOfAllTheStudentsInTHeClass());
+console.log(idsOfAllTheStudentsInTHeClass());
+console.log(subjectsOfASpecificStudent("Ravi"));
